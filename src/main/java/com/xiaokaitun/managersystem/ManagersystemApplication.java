f7b0b897ecad32568c98091a9fdd73c2703177ab
sunzhongkai <673158519@qq.com>
2022-04-27 15:09:47 +0800

    Initial commit

---
+++ src/main/java/com/xiaokaitun/managersystem/ManagersystemApplication.java
@@ -0,0 +1,13 @@
+package com.xiaokaitun.managersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagersystemApplication.class, args);
+    }
+
+}
